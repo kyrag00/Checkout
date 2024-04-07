994142f84c06217f88ee9c816584bde647a954b8
Kyra Getachew <kyragetachew@gmail.com>
2024-04-07 23:11:53 +0200

    server file + small changes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const cors = require("cors")
 require("dotenv").config()
+const cookieSession = require("cookie-session")
 
 const stripeRouter = require("./stripe/stripe.router")
+const userRouter = require("./users/users.router")
+const authRouter = require("./auth/auth.router")
+const productsRouter = require("./products/products.router")
 
 const app = express()
 
@@ -12,8 +16,17 @@ app.use(cors({
 }))
 
 app.use(express.json())
+app.use(
+    cookieSession({
+        secret: "s3cr3tk3y",
+        maxAge: 1000 * 60 * 60, // 1hour
+    })
+)
 
 app.use("/payments", stripeRouter)
+app.use("api/users", userRouter)
+app.use("api/auth", authRouter)
+app.use("/products", productsRouter)
 
 
 app.listen(3000, () => console.log("Server is up and running...💃"))
--- server/utils/fetchUsers.js
+++ server/utils/fetchUsers.js
@@ -1,7 +1,7 @@
 const fs = require("fs").promises
 
 const fetchUsers = async () => {
-    const data = await fs.readFile("./data/users.json")
+    const data = await fs.readFile("./users.json", "utf-8")
     const users = JSON.parse(data)
     return users
 }
