2af5cddb71c36c8a21546a39d1934e497e0900c8
Kyra Getachew <kyragetachew@gmail.com>
2024-04-12 12:11:16 +0200

    putting cart to navigation as its own page

--- client/src/Navigation.tsx
+++ client/src/Navigation.tsx
@@ -1,49 +1,18 @@
 import { NavLink } from "react-router-dom"
 import "./styles/nav.css"
-import Cart from "./Cart"
-import { useEffect, useState } from "react"
-import Login from "./Login"
-import Logout from "./Logout"
-import Register from "./Register"
 
 const Navigation = () => {
-
-    // const [user, setUser] = useState<string>("")
-
-    // useEffect(() => {
-    //     const authorize = async() => {
-    //       const response = await fetch("http://localhost:3000/api/auth/authorize", {
-    //     credentials: "include"
-    //   })
-    
-    //   const data = await response.json()
-    //   if (response.status === 200) {
-    //     setUser(data)
-    //   } else {
-    //     setUser("")
-    //   }
-    //     }
-    //     authorize()
-    //   }, [])
       
     return  (
         <>
         <nav className="navigation">
 
         <ul>
-            {/* <li>
-            {!user ? <Login setUser={setUser} /> : <Logout setUser={setUser} /> }
-        <br />
-        {!user && <Register />} */}
-            {/* </li> */}
-            {/* <li>
+            <li>
                 <NavLink to={"/"}>Home</NavLink>
             </li>
             <li>
-                <NavLink to={"/products"}>Products</NavLink>
-            </li> */}
-            <li>
-            <Cart/>
+                <NavLink to={"/cart"}>My Cart</NavLink>
             </li>
         </ul>
         {/* <Cart/> */}
--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Layout from "./pages/Layout";
 import Products from "./pages/Products";
 import Confirmation from "./pages/Confirmation";
+import Cart from "./Cart";
 
 export const router = createBrowserRouter([
     {
@@ -21,6 +22,10 @@ export const router = createBrowserRouter([
         {
           path:"/confirmation",
           element: <Confirmation/>
+        },
+        {
+          path: "/cart",
+          element: <Cart/>
         }
       ],
     }
