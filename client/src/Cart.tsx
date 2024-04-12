097388ed8ed7e64c5c5811dba4c37bf07f2c159e
Kyra Getachew <kyragetachew@gmail.com>
2024-04-12 13:39:30 +0200

    small changes

--- client/src/Cart.tsx
+++ client/src/Cart.tsx
@@ -1,16 +1,18 @@
 import Payment from "./Payment"
 import { useCart } from "./context/CartContext"
+import "./styles/products.css"
 
 const Cart = () => {
 
 const {cart} = useCart()
     return (
         <>
-        <div>
-            <h4>My Shopping Cart</h4>
+        <h1>My Shopping Cart</h1>
+        <div className="prod_container">
             {cart.map((item, index) => (
-                <div key={index}>
-                    <h5>{item.product.name}</h5>
+                <div className="product" key={index}>
+                    <h3>{item.product.name}</h3>
+                        <img src={item.product.images[0]} alt={item.product.name} />
                     <p>Price: {item.product.default_price.unit_amount / 100} SEK</p>
                     <p>Quantity: {item.quantity}</p>
                 </div>
